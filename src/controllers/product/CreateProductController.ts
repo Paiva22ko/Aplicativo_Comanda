7a79477a07e923f528e8da839af706b0d05effab
Paiva22ko <117324939+Paiva22ko@users.noreply.github.com>
2023-12-18 23:57:46 -0300

    primeiro commit

---
+++ src/controllers/product/CreateProductController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { CreateProductServices } from "../../services/product/CreateProductServices";
+
+class CreateProductController {
+  async handle(req: Request, res: Response) {
+    const { name, price, description, category_id } = req.body;
+
+    const createProductServices = new CreateProductServices();
+
+    if (!req.file) {
+      throw new Error("error upload file");
+    } else {
+      const { originalname, filename } = req.file;
+
+      console.log(filename);
+
+      const product = await createProductServices.execute({
+        name,
+        price,
+        description,
+        banner: "",
+        category_id,
+      });
+
+      return res.json(product);
+    }
+  }
+}
+
+export { CreateProductController };
